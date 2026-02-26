479482d581272729d180e141596357945904781f
shaun045 <shaun1adam2@gmail.com>
2026-02-27 04:44:45 +0800

    Updated Transaction UI

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/* ------------------>THIS IS FOR TRANSACTION SECTION<--------------------- */
+
+const transactionDropdown = document.getElementById("categoryDropdown");
+const makeTransaction = document.getElementById("transaction-btn");
+
+
+
+
+
+
+makeTransaction.addEventListener('click', () => {
+  console.log('transaction made');
+})
